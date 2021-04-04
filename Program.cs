e628c05f25548750a55418ea6a665080de9f2375
Troy Dorman <troydorman@gmail.com>
2021-04-03 20:31:53 -0600

    created the util class and re-factored the print employees method to it out of program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         {
             List<Employee> employees = GetEmployees();
-            PrintEmployees(employees);
+            Util.PrintEmployees(employees);
         }
 
         static List<Employee> GetEmployees()
@@ -38,13 +38,5 @@ namespace CatWorx.BadgeMaker
             return employees;
         }
 
-        static void PrintEmployees(List<Employee> employees)
-        {
-            for (int i = 0; i < employees.Count; i++)
-            {
-                string template = "{0,-10}\t{1,-20}\t{2}";
-                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-            }
-        }
     }
 }
