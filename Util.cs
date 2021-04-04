5ff4dd62c62927ddffd730800c71323c99f21126
Troy Dorman <troydorman@gmail.com>
2021-04-03 20:51:55 -0600

    wrote the MakeCSV method.  we are now writing our employee names to the file

--- Util.cs
+++ Util.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace CatWorx.BadgeMaker
 {
@@ -12,6 +13,28 @@ namespace CatWorx.BadgeMaker
             {
                 string template = "{0,-10}\t{1,-20}\t{2}";
                 Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+            }
+        }
+
+        public static void MakeCSV(List<Employee> employees)
+        {
+
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                // Loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // Write each employee to the file
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+                }
             }
         }
     }
