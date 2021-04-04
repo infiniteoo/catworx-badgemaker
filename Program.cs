2f5011e4e9b977b9218396d48935ccf3a8aa2572
Troy Dorman <troydorman@gmail.com>
2021-04-04 12:55:12 -0600

    added logic to allow user to choose between manual insertion of employee data or auto generation of random employee data

--- Program.cs
+++ Program.cs
@@ -7,15 +7,27 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            
-            List<Employee> employees = PeopleFetcher.GetFromApi();
+            List<Employee> employees;
+            Console.WriteLine("(M)anual or (A)uto Employee Generation (Auto is default): ");
+            // get a name from the console and assign it to a variable
+            string answer = Console.ReadLine();
+            if (answer == "m" || answer == "M")
+            {
+                employees = PeopleFetcher.GetEmployees();
+            }
+            else
+            {
+                employees = PeopleFetcher.GetFromApi();
+            }
+
+
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             Util.MakeBadges(employees);
 
         }
 
-        
+
 
     }
 }
