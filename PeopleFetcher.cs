6d267e559a78ec6e7ac315a66a5f004e89261d22
Troy Dorman <troydorman@gmail.com>
2021-04-04 12:44:56 -0600

    completed the data fetch method.  we grab json data as a string and convert it to a jobject.  then we iterate over it with a foreach and create a new employee with the various data

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
-namespace CatWorx.BadgeMaker {
+namespace CatWorx.BadgeMaker
+{
 
-class PeopleFetcher {
+    class PeopleFetcher
+    {
 
-    // code from GetEmployees() in Program.cs
-    public static List<Employee> GetEmployees()
+        // code from GetEmployees() in Program.cs
+        public static List<Employee> GetEmployees()
         {
             // I will return a List of strings
             List<Employee> employees = new List<Employee>();
@@ -32,7 +36,33 @@ class PeopleFetcher {
             }
             return employees;
         }
-}
+        public static List<Employee> GetFromApi()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (WebClient client = new WebClient())
+            {
+                string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                JObject json = JObject.Parse(response);
+                foreach (JToken token in json.SelectToken("results"))
+                {
+                    // Parse JSON data
+                    Employee emp = new Employee
+                    (
+                      token.SelectToken("name.first").ToString(),
+                      token.SelectToken("name.last").ToString(),
+                      Int32.Parse(token.SelectToken("id.value").ToString().Replace("-", "")),
+                      token.SelectToken("picture.large").ToString()
+                    );
+                    employees.Add(emp);
+                }
+
+
+
+            }
+            return employees;
+
+        }
+    }
 
 }
 
